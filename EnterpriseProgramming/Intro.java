fd8986592673e22fe9903be634c322707fade149
Gael <gaelthegoodboy@gmail.com>
2022-12-11 15:45:15 +0800

    Manual Java commit

---
+++ EnterpriseProgramming/Intro.java
@@ -0,0 +1,17 @@
+package EnterpriseProgramming;
+import java.util.*;
+
+public class Intro{
+    private String Name = "Gael";
+    private int Age = 19;
+    private String Specialization = "Enterprise Systems";
+    private String CareerPath = "Software Development";
+
+    public void Introduction(){
+        System.out.println(String.format("%s is age %d. He specializes in %s for the career of %s",Name,Age,Specialization,CareerPath));
+    }
+
+    public static void main(String args[]){
+        
+    }
+}
