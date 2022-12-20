92ee2ec79951d427a4ca6c12462644f9ff1d8305
Tomas Gael P. Estrera IV <Gamadan@Tomass-MacBook-Pro.local>
2022-12-21 01:34:37 +0800

    file

---
+++ Java/LearnJava/Method.java
@@ -0,0 +1,24 @@
+package LearnJava;
+
+//method name + method parameters = unique method signature
+
+public class Method {
+    static int Quant(int a, int b){
+        int c = a + b;
+        return c;
+    }
+
+    static int Quant(int a, int b, int c){
+        int d = a + b + c;
+        return d;
+    }
+
+    static int Quant(String a, String b){
+        int c = 1 + 2;
+        return c;
+    }
+
+    public static void main (String args[]){
+        System.out.println(Quant("Gael", "Estrera"));
+    }
+}
