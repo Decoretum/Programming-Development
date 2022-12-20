272dd0047c4d99ed559c3096551697cf515e90a0
Tomas Gael P. Estrera IV <Gamadan@Tomass-MacBook-Pro.local>
2022-12-20 17:43:01 +0800

    Mac to remote

---
+++ Java/LearnJava/Learn.java
@@ -0,0 +1,12 @@
+package LearnJava;
+
+public class Learn {
+    //Primitive (stores data, can hold 1 value) 
+    //Reference (User defined, stores address)
+    static String Name = "Gael";
+    static String University = "Ateneo de Manila University";
+
+    public static void main (String args[]){
+        System.out.println("The name of the owner of this program is " + Name);
+    }
+}
