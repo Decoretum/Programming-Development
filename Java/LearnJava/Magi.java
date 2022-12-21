cf06a6b36b727c89bf2748dbb96150fe9a86095f
Tomas Gael P. Estrera IV <Gamadan@Tomass-MacBook-Pro.local>
2022-12-21 20:43:00 +0800

    OOP

--- Java/LearnJava/Magi.java
+++ Java/LearnJava/Magi.java
@@ -1,26 +1,53 @@
 package LearnJava;
 
 public class Magi {
+    Weapon Weapon;
     String name;
     String Specialization;
     String Aura;
+    byte Charge;
     int Age;
-    Weapon Weapon;
 
-    Magi(String name, String spec, String aura, int age, Weapon wep){
+    Magi(String name, String spec, String aura, int age){
         this.name = name;
         this.Specialization = spec;
         this.Aura = aura;
         this.Age = age;
-        this.Weapon = wep;
         System.out.println(String.format("Magi %s has been initiated into this world.",this.name));
     }
 
+    void TakeWeapon(Weapon wep){
+        this.Weapon = wep;
+        System.out.println(String.format("The weapon '%s' has been acquired by %s.",this.Weapon.Name(),this.name));
+    }
     void BattleCry(){
         System.out.println(String.format("I, %s, shall devour you with my %s",this.name,this.Specialization));
     }
 
     void PrepWep(){
-        System.out.println(String.format("%s has charged his weapon '%s' with his power of %s!",this.name,this.Aura,this.Weapon.Name()));
+        if (this.Weapon != null){
+            System.out.println(String.format("%s has charged his weapon '%s' with his power of %s!",this.name,this.Aura,this.Weapon.Name()));
+            this.Charge = 1;
+        }
+        else{
+            System.out.println(String.format("%s currently has no weapon equipped, cannot prepare null weapon.",this.name));  
+        }
+        
     }
+
+    void Attack(){
+        if (this.Weapon != null){
+            if (this.Charge == 1){
+                System.out.println(String.format("%s launches %s bolts with the %s!",this.name,this.Aura,this.Weapon.Name()));
+            }
+            else{
+                System.out.println(String.format("The %s has not been charged yet.",this.Weapon.Name()));
+            }
+        }
+        else{
+            System.out.println(String.format("%s currently has no weapon equipped, cannot attack.",this.name));
+        }
+        
+    }   
+    
 }
--- Java/LearnJava/OOP.java
+++ Java/LearnJava/OOP.java
@@ -3,9 +3,11 @@ package LearnJava;
 public class OOP {
     public static void main (String args[]){
         Weapon ChaosStaff = new Weapon("Staff of Chaos","Abyss");
-        Magi Gael = new Magi("Gael", "Destruction & Alteration", "Abyss", 19, ChaosStaff);
+        Magi Gael = new Magi("Gael", "Destruction & Alteration", "Abyss", 19);
         Gael.BattleCry();
         Gael.PrepWep();
+        Gael.Attack();
+        Gael.TakeWeapon(ChaosStaff);
     }
     
 
