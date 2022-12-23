fe3cc4472184d6f590f5309685e53af66e10c171
Tomas Gael P. Estrera IV <Gamadan@Tomass-MacBook-Pro.local>
2022-12-23 13:52:36 +0800

    Started new web application

--- Web/SalesApp/Inventory/views.py
+++ Web/SalesApp/Inventory/views.py
@@ -1,11 +1,12 @@
 from django.contrib import messages
 from django.shortcuts import render,redirect,get_object_or_404
-from .models import Position,Candidate,Userperson, Vote
+#from .models import Position,Candidate,Userperson, Vote
 from django.contrib.auth import authenticate,login
-from django.contrib.auth.models import auth, User
+#from django.contrib.auth.models import auth, User
 from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
-def signup(request):
+def home(request):
+    return render(request, 'Inventory/home.html')
     
--- Web/SalesApp/SalesApp/settings.py
+++ Web/SalesApp/SalesApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Inventory'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'SalesApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'invdb',
+        'USER' : 'root',
+        'PASSWORD' : 'Cubil97823',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
     }
 }
 
