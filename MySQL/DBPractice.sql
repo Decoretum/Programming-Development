2e1bef5f30034cdca5e214caa42b39728059dc6f
Gael <gaelthegoodboy@gmail.com>
2023-01-18 01:48:10 +0800

    MySQL Practice and Mastery increment

--- MySQL/DBPractice.sql
+++ MySQL/DBPractice.sql
@@ -4,6 +4,13 @@ show tables;
 desc programmers;
 select * from programmers;
 
+#setting a foreign key for a table
+alter table game
+add constraint Comp_fk
+foreign key (Company) references company(company_name);
+
+#updating and adding new column in table
+
 update programmers
 set Coursegraduated = "BS CS"
 where nickname = "Justin";
@@ -11,6 +18,8 @@ where nickname = "Justin";
 select * from programmers;
 desc programmers;
 desc techskills;
+
+#here
 
 #3 types of DML: update, delete, select
 
@@ -100,4 +109,71 @@ where (Genre = "Sandbox") and Platform = "Multiform";
 select * from game
 where (Platform = "Multiform") and Genre = "Sandbox";
 
+show tables;
 
+select max(NoteID)
+from notes
+where NoteHeader like "%Prog%";
+
+select min(NoteID)
+from notes
+where NoteHeader like "%Prog%";
+
+select * from notes;
+
+#count, avg, sum
+select count(NoteHeader) from notes;
+
+desc game;
+
+#left, right, inner joins, cross joins
+#inner joins, for values found on both tables
+select game.name, company.company_name
+from game
+inner join company
+on game.Company = company.company_name;
+
+
+alter table company
+add GameTheme varchar(20) after company_location;
+
+alter table company
+add FoundDate varchar(30) after GameTheme;
+
+desc company;
+select * from company;
+
+update company
+set GameTheme = "MMORPG"
+where company_name = "Abyssal Sage";
+
+update company
+set FoundDate = "11/25/2022"
+where company_name = "Abyssal Sage";
+
+select * from company;
+
+alter table company
+modify GameTheme varchar(20) not null;
+
+alter table company
+modify FoundDate varchar(30) not null;
+
+desc company;
+desc game;
+
+alter table game #Renaming/Changing column name in table
+change column Endorser Company varchar(20);
+
+desc company;
+insert into company 
+values 
+("Bandai","Japan","JRPG","1/5/1980"),
+("Valve","USA","Various Genre","4/2/1993"),
+("Mojang","Sweden","Sandbox","2/5/2004"),
+("Redit","USA","Sandbox","5/6/2008");
+
+insert into company
+values ("Bethesda","USA","Sandbox","4/2/1989");
+
+select * from company;
