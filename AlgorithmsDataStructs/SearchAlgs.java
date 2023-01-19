750e5ef973d9fab7d3d9a2470d624bd765657779
Gael <gaelthegoodboy@gmail.com>
2023-01-19 10:46:25 +0800

    Files

---
+++ AlgorithmsDataStructs/SearchAlgs.java
@@ -0,0 +1,9 @@
+//https://www.tutorialspoint.com/data_structures_algorithms/index.htm
+
+package AlgorithmsDataStructs;
+
+public class SearchAlgs{
+    public static void main (String args[]){
+        
+    }
+}
---
+++ AlgorithmsDataStructs/SortAlgs.java
@@ -0,0 +1,8 @@
+//https://www.tutorialspoint.com/data_structures_algorithms/index.htm
+package AlgorithmsDataStructs;
+
+public class SortAlgs{
+    public static void main (String args[]){
+        
+    }
+}
