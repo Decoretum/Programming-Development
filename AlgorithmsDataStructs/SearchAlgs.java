b490dbdead51c3b1222a3ada94970fe4169ba1d0
Gael Estrera <gaelestrera@gmail.com>
2023-01-26 14:19:15 +0800

    SelSort

--- AlgorithmsDataStructs/SearchAlgs.java
+++ AlgorithmsDataStructs/SearchAlgs.java
@@ -3,7 +3,16 @@
 package AlgorithmsDataStructs;
 
 public class SearchAlgs{
+    static int LinSort(int array[], int target){
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == target){
+                return i;
+            }
+        }
+        return 1;
+    }
     public static void main (String args[]){
-        
+        int [] array = {1,2,3,4,5};
+        System.out.println(LinSort(array,3));
     }
 }
--- AlgorithmsDataStructs/SortAlgs.java
+++ AlgorithmsDataStructs/SortAlgs.java
@@ -2,16 +2,37 @@
 package AlgorithmsDataStructs;
 
 public class SortAlgs{
-    static int LinSort(int array[], int target){
-        for (int i = 0; i < array.length; i++){
-            if (array[i] == target){
-                return i;
+
+    static void SelectionSort(int array[]){
+        int alength = array.length;
+        for (int i = 0; i < alength; i++){
+            int min = i;
+            for (int j = i+1; j < alength; j++){
+                if (array[min] > array[j]){
+                    min = j;
+                }
             }
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
+
         }
-        return 1;
     }
+
+    /*Selection Sort
+    Time Complexity = O(n^2) since there are two nested loops 
+    Space Complexity
+    Auxiliary Space: O(1) as the only extra memory used is for temporary variables
+    while swapping two values in Array. The selection sort never makes more than O(N) 
+    swaps and can be useful when memory write is a costly operation.
+
+    O(1) space complexity since space does not grow no matter the operations, loops, or input of data
+    */
     public static void main (String args[]){
-        int [] array = {1,2,3,4,5};
-        System.out.println(LinSort(array,3));
+        int [] array = {4,1,2,5,6};
+        SelectionSort(array);
+        for (int i =0; i<array.length; i++){
+            System.out.print(array[i]);
+        }
     }
 }
