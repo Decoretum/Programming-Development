86d5fdf16cc95cd8552ba9273a04f2db825f4677
Gael <gaelthegoodboy@gmail.com>
2023-02-17 02:24:04 +0800

    Leetcode biased

--- Leetcode/Arrays.java
+++ Leetcode/Arrays.java
@@ -26,15 +26,72 @@ public class Arrays{
         }
 
     }
-    public static void main (String args[]){
-        int [] a1 = new int[10];
-        for (int i = 0; i < a1.length; i++){
-            a1[i] = i;
+
+    static int removeElement(int[]nums, int val){
+        int count = 0;
+        int ccount = 0;
+        int proper = 0;
+        boolean equal = true;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == val){
+                nums[i] = 51;
+                count ++;
+            }
+            else{
+                equal = false;
+            }
         }
-        ShiftArrayLeft(a1,4); //2 elements shifted
-        for (int i : a1){
-            System.out.println(i);
+        
+        boolean Condition = true;
+        int i = 0; //start
+        int j = nums.length-1; //end
+        while (ccount != count){
+            if (equal == true){
+                ccount = count;
+            }
+
+            else if (i == j){
+                break;
+            }
+
+            else{
+                if(nums[i] != 51){
+                    i++;
+                }
+                else if (nums[i] == 51){
+                    if (nums[j] == 51){
+                        j--;
+                    }
+                    else{
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        i++;
+                        j = nums.length-1;
+                        ccount++;
+                    }
+                }
+            }
+           
+
         }
 
+        for (int s : nums){
+            if (s != 51){
+                proper++;
+            }
+        }
+        return proper;
+    }
+
+    public static void main (String args[]){
+        int [] a1 = new int[10];
+        int[] a2 = {0,1,2,2,3,0,4,2};
+
+        System.out.println(removeElement(a2,2));
+        for (int i : a2){
+            System.out.println(i);
+        }
+        
     }
 }
