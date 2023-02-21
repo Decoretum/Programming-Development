ffeb4bb1fac48939d2d4521443a586e28701a431
Gael Estrera <gaelestrera@gmail.com>
2023-02-21 13:52:50 +0800

    Learning Vue

---
+++ Javascript/Vue/VueLearning/src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue' //import ability to create app
+import { createPinia } from 'pinia'
+
+// components for app
+// we import components from a single-file component
+
+import App from './App.vue' //root components
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp({
+    testy(){
+        return{
+            lar : "Gael"
+        }
+    }
+})
+    
+     //instance of application
+
+app.use(createPinia())
+app.use(router)
+
+
+//we mount the app to a DOM element or selector string
+app.mount('#app')
---
+++ Javascript/Vue/VueLearning/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <span class=""> {{ lar }}</span> 
+  </main>
+</template>
